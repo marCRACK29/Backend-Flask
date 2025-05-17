c5f7c5bf7a26fabf3d40bf0ae8111f8768dc6350
marCRACK29 <mamartinez2023@udec.cl>
2025-05-17 17:23:04 -0400

    agrega .venv al .gitignore

--- tests/test_registro_usr.py
+++ tests/test_registro_usr.py
@@ -3,10 +3,10 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 nuevo_usuario = {
-    "RUT": "12.345.678-9",
-    "nombre": "Juan Pérez",
-    "correo": "juan@example.com",
-    "contraseña": "secreta123"
+    "RUT": "21.123.123-4",
+    "nombre": "Sebastián Vega",
+    "correo": "seba@example.com",
+    "contraseña": "holaaa"
 }
 
 response = requests.post(BASE + "api/auth/register", json=nuevo_usuario)
